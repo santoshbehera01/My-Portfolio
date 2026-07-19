1cf038f8c1faa8bc8019574ebcfcbb495e64df34
Santosh <santoshkumarbehera049@gmail.com>
2026-07-19 12:08:03 +0530

    Update the about section

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -73,7 +73,7 @@ const AboutSection = () => {
               <p>
                 Hi there! I'm <span className="text-foreground font-medium">Santosh Kumar Behera</span>, 
                 a B.Tech Computer Science student focused on building impactful software solutions and exploring modern technologies.
-                I enjoy developing real-world projects in AI and web development, with hands on experience in applications such as an AI Resume Analyzer and other development focused projects.
+                I enjoy developing real world projects in AI and web development, with hands on experience in applications such as an AI Resume Analyzer and other development focused projects.
               </p>
               <p>I actively participate in technical events and project exhibitions to strengthen my technical, analytical, and problem-solving skills.
                 Driven by continuous learning and innovation, I am always eager to explore new technologies, work on meaningful projects, and grow as a future Software Engineer.
